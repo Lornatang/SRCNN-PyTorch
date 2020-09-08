1d8c228d2da3051eb2d7f03b223b008db29b79c6
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-08 15:24:25 +0800

    Create model.py

---
+++ srcnn_pytorch/model.py
@@ -0,0 +1,32 @@
+# Copyright 2020 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.cuda import amp
+
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
+
+    @amp.autocast()
+    def forward(self, inputs):
+        out = F.relu(self.conv1(inputs))
+        out = F.relu(self.conv2(out))
+        out = self.conv3(out)
+
+        return out
