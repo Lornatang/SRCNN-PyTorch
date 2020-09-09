cd546ffdab6f29fe3014e61c4fa84e6cf3bfdfa8
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-09 08:09:41 +0800

    Create test_benchmark.py

---
+++ test_benchmark.py
@@ -0,0 +1,90 @@
+# Copyright 2020 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import argparse
+import math
+import os
+import random
+
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.utils.data
+import torch.utils.data.distributed
+
+from srcnn_pytorch import DatasetFromFolder
+from srcnn_pytorch import SRCNN
+
+parser = argparse.ArgumentParser(description="PyTorch Super Resolution CNN.")
+parser.add_argument("--dataroot", type=str, default="./data/DIV2K",
+                    help="Path to datasets. (default:`./data/DIV2K`)")
+parser.add_argument("-j", "--workers", default=0, type=int, metavar="N",
+                    help="Number of data loading workers. (default:0)")
+parser.add_argument("-b", "--batch-size", default=8, type=int,
+                    metavar="N",
+                    help="mini-batch size (default: 8), this is the total "
+                         "batch size of all GPUs on the current node when "
+                         "using Data Parallel or Distributed Data Parallel.")
+parser.add_argument("--scale-factor", type=int, default=4,
+                    help="Low to high resolution scaling factor. (default:4).")
+parser.add_argument("--cuda", action="store_true", help="Enables cuda")
+parser.add_argument("--weights", type=str, required=True,
+                    help="Path to weights.")
+parser.add_argument("--manualSeed", type=int,
+                    help="Seed for initializing training. (default:none)")
+
+args = parser.parse_args()
+print(args)
+
+try:
+    os.makedirs(args.outf)
+except OSError:
+    pass
+
+if args.manualSeed is None:
+    args.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", args.manualSeed)
+random.seed(args.manualSeed)
+torch.manual_seed(args.manualSeed)
+
+cudnn.benchmark = True
+
+if torch.cuda.is_available() and not args.cuda:
+    print("WARNING: You have a CUDA device, "
+          "so you should probably run with --cuda")
+
+dataset = DatasetFromFolder(f"{args.dataroot}/val",
+                            scale_factor=args.scale_factor)
+
+dataloader = torch.utils.data.DataLoader(dataset,
+                                         batch_size=1,
+                                         shuffle=False, pin_memory=True,
+                                         num_workers=int(args.workers))
+
+device = torch.device("cuda:0" if args.cuda else "cpu")
+
+model = SRCNN().to(device)
+
+model.load_state_dict(torch.load(args.weights, map_location=device))
+criterion = nn.MSELoss().to(device)
+
+# Test
+avg_psnr = 0
+with torch.no_grad():
+    for data in dataloader:
+        inputs, target = data[0].to(device), data[1].to(device)
+
+        out = model(inputs)
+        loss = criterion(out, target)
+        psnr = 10 * math.log10(1 / loss.item())
+        avg_psnr += psnr
+print(f"EVal average PSNR: {avg_psnr / len(dataloader):.2f} dB.")
