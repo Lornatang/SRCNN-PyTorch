b6b96cb887a2ac543f0de27d7e9aa2d078e8988f
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-24 12:30:21 +0800

    Update download_weights.sh

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start downloading pre training model..."
-wget https://github.com/Lornatang/SRGAN-PyTorch/releases/download/1.1/srcnn_2x.pth
-wget https://github.com/Lornatang/SRGAN-PyTorch/releases/download/1.1/srcnn_3x.pth
-wget https://github.com/Lornatang/SRGAN-PyTorch/releases/download/1.1/srcnn_4x.pth
+wget https://github.com/Lornatang/SRCNN-PyTorch/releases/download/1.1/srcnn_2x.pth
+wget https://github.com/Lornatang/SRCNN-PyTorch/releases/download/1.1/srcnn_3x.pth
+wget https://github.com/Lornatang/SRCNN-PyTorch/releases/download/1.1/srcnn_4x.pth
 echo "All pre training models have been downloaded!"
