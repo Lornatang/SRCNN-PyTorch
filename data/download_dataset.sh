54fa0461e72805ebccf88c1168b3ace00ffc9655
Liu Changyu <liuchangyu1111@gmail.com>
2020-09-24 09:21:20 +0800

    Update download_dataset.sh

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -2,9 +2,14 @@
 
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
+
 unzip DIV2K_train_HR.zip
 unzip DIV2K_valid_HR.zip
-mv DIV2K_train_HR train
-mv DIV2K_valid_HR val
+
+mkdir DIV2K
+
+mv DIV2K_train_HR DIV2K/train
+mv DIV2K_valid_HR DIV2K/val
+
 rm DIV2K_train_HR.zip
 rm DIV2K_valid_HR.zip
