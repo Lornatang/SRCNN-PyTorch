96a17464d7a42c5440db57701b8158cd91baa21f
Liu Changyu <liuchangyu1111@gmail.com>
2021-10-27 19:04:27 +0800

    Update train.py

--- train.py
+++ train.py
@@ -12,119 +12,170 @@
 # limitations under the License.
 # ============================================================================
 """File description: Realize the model training function."""
+import os
+
+import torch
+from torch import nn
+from torch import optim
 from torch.cuda import amp
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
-from config import *
-from dataset import BaseDataset
+import config
+from dataset import LMDBDataset
+from model import SRCNN
 
 
-def train(train_dataloader, epoch, scaler) -> None:
-    """Training a super-resolution model based on the MSE loss function.
+def load_dataset() -> [DataLoader, DataLoader]:
+    train_datasets = LMDBDataset(config.train_lr_lmdb_path, config.train_hr_lmdb_path)
+    valid_datasets = LMDBDataset(config.valid_lr_lmdb_path, config.valid_hr_lmdb_path)
+    train_dataloader = DataLoader(train_datasets, config.batch_size, shuffle=True, pin_memory=True)
+    valid_dataloader = DataLoader(valid_datasets, config.batch_size, shuffle=False, pin_memory=True)
 
-    Args:
-        train_dataloader (torch.utils.data.DataLoader): The loader of the training dataset.
-        epoch (int): number of training cycles.
-        scaler (amp.GradScaler): Gradient scaler.
-    """
-    # The number of training steps per cycle.
+    return train_dataloader, valid_dataloader
+
+
+def build_model() -> nn.Module:
+    model = SRCNN(mode="train").to(config.device, non_blocking=True)
+
+    return model
+
+
+def define_loss() -> nn.MSELoss:
+    criterion = nn.MSELoss().to(config.device, non_blocking=True)
+
+    return criterion
+
+
+def define_optimizer(model) -> optim.SGD:
+    if config.model_optimizer_name == "sgd":
+        optimizer = optim.SGD([{"params": model.features.parameters()},
+                               {"params": model.map.parameters()},
+                               {"params": model.reconstruction.parameters(), "lr": config.model_lr * 0.1}],
+                              lr=config.model_lr)
+    else:
+        optimizer = optim.SGD([{"params": model.features.parameters()},
+                               {"params": model.map.parameters()},
+                               {"params": model.reconstruction.parameters(), "lr": config.model_lr * 0.1}],
+                              lr=config.model_lr)
+
+    return optimizer
+
+
+def resume_checkpoint(model):
+    if config.resume:
+        if config.resume_weight != "":
+            model.load_state_dict(torch.load(config.resume_weight), strict=config.strict)
+
+
+def train(model, train_dataloader, criterion, optimizer, epoch, scaler, writer) -> None:
+    # Calculate how many iterations there are under epoch
     batches = len(train_dataloader)
-    # Set the module to training mode.
+    # Put the generator in training mode
     model.train()
-    for index, (lr, hr) in enumerate(train_dataloader):
-        lr = lr.to(device)
-        hr = hr.to(device)
 
-        # Initialize the model gradient.
+    for index, (lr, hr) in enumerate(train_dataloader):
+        lr = lr.to(config.device, non_blocking=True)
+        hr = hr.to(config.device, non_blocking=True)
+
+        # Initialize the generator gradient
         model.zero_grad()
-        # Mixed precision training.
+
+        # Mixed precision training
         with amp.autocast():
             sr = model(lr)
             loss = criterion(sr, hr)
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
-        # Write the loss during training to Tensorboard.
-        iters = index + epoch * batches + 1
-        writer.add_scalar("Train/MSE_Loss", loss.item(), iters)
-        # Print loss function.
-        if (index + 1) % 10 == 0 or (index + 1) == batches:
-            print(f"Epoch[{epoch + 1:04d}/{epochs:04d}]({index + 1:05d}/{batches:05d}) "
-                  f"Loss: {loss.item():.6f}.")
+
+        # In this Epoch, every one hundred iterations and the last iteration print the loss function
+        # and write it to Tensorboard at the same time
+        if (index + 1) % 100 == 0 or (index + 1) == batches:
+            iters = index + epoch * batches + 1
+            writer.add_scalar("Train/MSE_Loss", loss.item(), iters)
+            print(f"Epoch[{epoch + 1:05d}/{config.epochs:05d}]({index + 1:05d}/{batches:05d}) MSE loss: {loss.item():.6f} .")
 
 
-def validate(valid_dataloader, epoch) -> float:
-    """Test the performance of the super-resolution model.
-
-    Args:
-        valid_dataloader (torch.utils.data.DataLoader): loader for validating data set.
-        epoch (int): number of training cycles.
-
-    Returns:
-        PSNR value(float).
-    """
-    # The number of verification steps per cycle.
+def validate(model, valid_dataloader, criterion, epoch, writer) -> float:
+    # Calculate how many iterations there are under Epoch.
     batches = len(valid_dataloader)
-    # Set the module to verification mode.
+    # Put the generator in verification mode.
     model.eval()
     # Initialize the evaluation index.
-    total_psnr_value = 0.0
+    total_psnr_metric = 0.0
 
     with torch.no_grad():
         for index, (lr, hr) in enumerate(valid_dataloader):
-            lr = lr.to(device)
-            hr = hr.to(device)
-            # Calculate the PSNR value.
+            lr = lr.to(config.device, non_blocking=True)
+            hr = hr.to(config.device, non_blocking=True)
+            # Calculate the PSNR evaluation index.
             sr = model(lr)
-            psnr_value = 10 * torch.log10(1 / criterion(sr, hr)).item()
-            total_psnr_value += psnr_value
+            psnr = 10 * torch.log10(1 / criterion(sr, hr)).item()
+            total_psnr_metric += psnr
 
-        avg_psnr_value = total_psnr_value / batches
-        # Write the PSNR indicator value into Tensorboard.
-        writer.add_scalar("Valid/MSE_metrics", avg_psnr_value, epoch + 1)
+        avg_psnr = total_psnr_metric / batches
+        # Write the value of each round of verification indicators into Tensorboard.
+        writer.add_scalar("Valid/PSNR", avg_psnr, epoch + 1)
         # Print evaluation indicators.
-        print(f"Valid Epoch[{epoch + 1:04d}] avg PSNR: {avg_psnr_value:.2f}.\n")
+        print(f"Epoch[{epoch + 1:04d}] avg PSNR: {avg_psnr:.2f}.\n")
 
-    return avg_psnr_value
+    return avg_psnr
 
 
 def main() -> None:
-    # Create a super-resolution experiment result folder.
-    if not os.path.exists(exp_dir1):
-        os.makedirs(exp_dir1)
-    if not os.path.exists(exp_dir2):
-        os.makedirs(exp_dir2)
+    # Create a folder of super-resolution experiment results
+    samples_dir = os.path.join("samples", config.exp_name)
+    results_dir = os.path.join("results", config.exp_name)
+    if not os.path.exists(samples_dir):
+        os.makedirs(samples_dir)
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
 
-    # Load the dataset.
-    train_dataset = BaseDataset(train_dir)
-    valid_dataset = BaseDataset(valid_dir)
-    train_dataloader = DataLoader(train_dataset, batch_size, True, pin_memory=True)
-    valid_dataloader = DataLoader(valid_dataset, batch_size, False, pin_memory=True)
+    # Create training process log file
+    writer = SummaryWriter(os.path.join("samples", "logs", config.exp_name))
 
-    # Check whether the training progress of the last abnormal end is restored, for example, the power is cut off in the middle of the training.
-    if resume:
-        print("Resuming...")
-        model.load_state_dict(torch.load(resume_weight, map_location=device))
+    print("Load train dataset and valid dataset...")
+    train_dataloader, valid_dataloader = load_dataset()
+    print("Load train dataset and valid dataset successfully.")
 
-    # Create GradScalar function.
+    print("Build SR model...")
+    model = build_model()
+    print("Build SR model successfully.")
+
+    print("Define all loss functions...")
+    criterion = define_loss()
+    print("Define all loss functions successfully.")
+
+    print("Define all optimizer functions...")
+    optimizer = define_optimizer(model)
+    print("Define all optimizer functions successfully.")
+
+    print("Check whether the training weight is restored...")
+    resume_checkpoint(model)
+    print("Check whether the training weight is restored successfully.")
+
+    # Initialize the gradient scaler
     scaler = amp.GradScaler()
 
-    # Initialize the evaluation index.
-    best_mse_metrics = 0.0
+    # Initialize training to generate network evaluation indicators
+    best_psnr = 0.0
 
-    # Train the super-resolution model based on the MSE loss function.
-    for epoch in range(start_epoch, epochs):
-        train(train_dataloader, epoch, scaler)
-        psnr_value = validate(valid_dataloader, epoch)
-        # Automatically search and save the optimal model weight.
-        is_best = psnr_value > best_mse_metrics
-        best_mse_metrics = max(psnr_value, best_mse_metrics)
-        torch.save(model.state_dict(), os.path.join(exp_dir1, f"epoch_{epoch + 1}.pth"))
+    print("Start train SRCNN model.")
+    for epoch in range(config.start_epoch, config.epochs):
+        train(model, train_dataloader, criterion, optimizer, epoch, scaler, writer)
+
+        psnr = validate(model, valid_dataloader, criterion, epoch, writer)
+        # Automatically save the model with the highest index
+        is_best = psnr > best_psnr
+        best_psnr = max(psnr, best_psnr)
+        torch.save(model.state_dict(), os.path.join(samples_dir, f"srcnn_epoch{epoch + 1}.pth"))
         if is_best:
-            torch.save(model.state_dict(), os.path.join(exp_dir2, "best.pth"))
+            torch.save(model.state_dict(), os.path.join(results_dir, "srcnn_best.pth"))
 
-    # Save the model weight of the last cycle in the process of training the super-resolution model.
-    torch.save(model.state_dict(), os.path.join(exp_dir2, "last.pth"))
+    # Save the generator weight under the last Epoch in this stage
+    torch.save(model.state_dict(), os.path.join(results_dir, "srcnn_last.pth"))
+    print("End train SRCNN model.")
 
 
 if __name__ == "__main__":
