94b20a006943e27049eed044d0d5e59365cc4d9b
Liu Changyu <liuchangyu1111@gmail.com>
2021-11-11 17:27:15 +0800

    Update create_lmdb_dataset.py

--- scripts/create_lmdb_dataset.py
+++ scripts/create_lmdb_dataset.py
@@ -21,73 +21,61 @@ from tqdm import tqdm
 
 
 def main(args):
-    if os.path.exists(args.lr_lmdb_path):
-        shutil.rmtree(args.lr_lmdb_path)
-    if os.path.exists(args.hr_lmdb_path):
-        shutil.rmtree(args.hr_lmdb_path)
+    if os.path.exists(args.lmdb_path):
+        shutil.rmtree(args.lmdb_path)
 
-    os.makedirs(args.lr_lmdb_path)
-    os.makedirs(args.hr_lmdb_path)
+    os.makedirs(args.lmdb_path)
 
-    image_file_names = os.listdir(args.lr_image_dir)
+    image_file_names = os.listdir(args.image_dir)
     total_image_number = len(image_file_names)
 
     # Determine the LMDB database file size according to the image size
-    lr_image = cv2.imread(os.path.abspath(f"{args.lr_image_dir}/{image_file_names[0]}"))
-    hr_image = cv2.imread(os.path.abspath(f"{args.lr_image_dir}/{image_file_names[0]}"))
-    lr_image_lmdb_map_size = lr_image.shape[0] * lr_image.shape[1] * lr_image.shape[2] * total_image_number * 1.5
-    hr_image_lmdb_map_size = hr_image.shape[0] * hr_image.shape[1] * hr_image.shape[2] * total_image_number * 1.5
+    image = cv2.imread(os.path.abspath(f"{args.image_dir}/{image_file_names[0]}"))
+    image_lmdb_map_size = image.shape[0] * image.shape[1] * image.shape[2] * total_image_number * 1.5
 
     # Open LMDB write environment
-    lr_lmdb_env = lmdb.open(args.lr_lmdb_path, map_size=int(lr_image_lmdb_map_size))
-    hr_lmdb_env = lmdb.open(args.hr_lmdb_path, map_size=int(hr_image_lmdb_map_size))
+    lmdb_env = lmdb.open(args.lmdb_path, map_size=int(image_lmdb_map_size))
 
     # Easy to read and visualize with DataLoader
     total_sub_image_number = 1
     process_bar = tqdm(image_file_names, total=total_image_number)
 
     # Start over to write the file
-    lr_content = lr_lmdb_env.begin(write=True)
-    hr_content = hr_lmdb_env.begin(write=True)
+    content = lmdb_env.begin(write=True)
 
     for image_file_name in process_bar:
         # Use OpenCV to read low-resolution and high-resolution images
-        lr_image = cv2.imread(f"{args.lr_image_dir}/{image_file_name}")
-        hr_image = cv2.imread(f"{args.hr_image_dir}/{image_file_name}")
-        lr_image = cv2.cvtColor(lr_image, cv2.COLOR_BGR2RGB)
-        hr_image = cv2.cvtColor(hr_image, cv2.COLOR_BGR2RGB)
+        image = cv2.imread(f"{args.image_dir}/{image_file_name}")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Process HR to LR image
+        # image = cv2.resize(image, [33 // 3, 33 // 3], interpolation=cv2.INTER_CUBIC)
+        # image = cv2.resize(image, [33, 33], interpolation=cv2.INTER_CUBIC)
 
         # Label from int to ascii
         image_key_bytes = str(total_sub_image_number).encode("ascii")
 
         # LR bytes and HR bytes
-        _, lr_image_encode = cv2.imencode(".bmp", lr_image)
-        _, hr_image_encode = cv2.imencode(".bmp", hr_image)
-        lr_image_bytes = lr_image_encode.tobytes()
-        hr_image_bytes = hr_image_encode.tobytes()
+        _, image_encode = cv2.imencode(".png", image)
+        image_bytes = image_encode.tobytes()
 
         process_bar.set_description(f"Write {total_sub_image_number} images to lmdb dataset.")
         total_sub_image_number += 1
 
-        lr_content.put(image_key_bytes, lr_image_bytes)
-        hr_content.put(image_key_bytes, hr_image_bytes)
+        content.put(image_key_bytes, image_bytes)
 
     # Submit image data to LMDB database
-    lr_content.commit()
-    hr_content.commit()
+    content.commit()
     # Close all events
     process_bar.close()
-    lr_lmdb_env.close()
-    hr_lmdb_env.close()
+    lmdb_env.close()
     print("Writing lmdb database successfully.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create LMDB database scripts.")
-    parser.add_argument("--lr_image_dir", type=str, required=True, help="Path to low-resolution image directory.")
-    parser.add_argument("--hr_image_dir", type=str, required=True, help="Path to high-resolution image directory.")
-    parser.add_argument("--lr_lmdb_path", type=str, required=True, help="Path to low-resolution lmdb database.")
-    parser.add_argument("--hr_lmdb_path", type=str, required=True, help="Path to high-resolution lmdb database.")
+    parser.add_argument("--image_dir", type=str, default="T91/train", help="Path to image directory. (Default: ``T91/train``)")
+    parser.add_argument("--lmdb_path", type=str, default="train_lmdb/T91_LRbicx2_lmdb", help="Path to lmdb database. (Default: ``train_lmdb/T91_LRbicx2_lmdb``)")
     args = parser.parse_args()
 
     main(args)
