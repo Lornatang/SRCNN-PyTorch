c178aa51fdf0d99a785711b641966c669447b35b
Liu Changyu <liuchangyu1111@gmail.com>
2021-11-17 19:04:29 +0800

    Update validate.py

--- validate.py
+++ validate.py
@@ -60,25 +60,33 @@ def main() -> None:
 
         print(f"Processing `{os.path.abspath(hr_image_path)}`...")
         # Make low-resolution images.
-        image = Image.open(hr_image_path).convert("RGB")
-        image_width = (image.width // config.upscale_factor) * config.upscale_factor
-        image_height = (image.height // config.upscale_factor) * config.upscale_factor
-        image = image.resize([image_width, image_height], Image.BICUBIC)
-        image = image.resize([image.width // config.upscale_factor, image.height // config.upscale_factor], Image.BICUBIC)
-        image = image.resize([image.width * config.upscale_factor, image.height * config.upscale_factor], Image.BICUBIC)
-        # Extract Y channel image data.
-        lr_image = np.array(image).astype(np.float32)
+        hr_image = Image.open(hr_image_path).convert("RGB")
+        hr_image_width = (hr_image.width // config.upscale_factor) * config.upscale_factor
+        hr_image_height = (hr_image.height // config.upscale_factor) * config.upscale_factor
+        hr_image = hr_image.resize([hr_image_width, hr_image_height], Image.BICUBIC)
+
+        lr_image = hr_image.resize([hr_image.width // config.upscale_factor, hr_image.height // config.upscale_factor], Image.BICUBIC)
+        lr_image = lr_image.resize([lr_image.width * config.upscale_factor, lr_image.height * config.upscale_factor], Image.BICUBIC)
+        # Extract Y channel lr image data.
+        lr_image = np.array(lr_image).astype(np.float32)
         lr_ycbcr = imgproc.convert_rgb_to_ycbcr(lr_image)
         lr_image_y = lr_ycbcr[..., 0]
         lr_image_y /= 255.
         lr_tensor_y = torch.from_numpy(lr_image_y).to(config.device).unsqueeze(0).unsqueeze(0)
         lr_tensor_y = lr_tensor_y.half()
+        # Extract Y channel hr image data.
+        hr_image = np.array(hr_image).astype(np.float32)
+        hr_ycbcr = imgproc.convert_rgb_to_ycbcr(hr_image)
+        hr_image_y = hr_ycbcr[..., 0]
+        hr_image_y /= 255.
+        hr_tensor_y = torch.from_numpy(hr_image_y).to(config.device).unsqueeze(0).unsqueeze(0)
+        hr_tensor_y = hr_tensor_y.half()
         # Only reconstruct the Y channel image data.
         with torch.no_grad():
             sr_tensor_y = model(lr_tensor_y).clamp_(0.0, 1.0)
 
         # Cal PSNR
-        total_psnr += 10. * torch.log10(1. / torch.mean((sr_tensor_y - lr_tensor_y) ** 2))
+        total_psnr += 10. * torch.log10(1. / torch.mean((sr_tensor_y - hr_tensor_y) ** 2))
 
         sr_image_y = sr_tensor_y.mul_(255.0).cpu().squeeze_(0).squeeze_(0).numpy()
         sr_image = np.array([sr_image_y, lr_ycbcr[..., 1], lr_ycbcr[..., 2]]).transpose([1, 2, 0])
